bdd299f0ea4f7d7de46a52bd5fe3131a2264dfde
Guillenzo-hub <GZ22006@ues.edu.sv>
2025-05-26 17:13:33 -0600

    fix Auth0

--- src/main/java/com/example/rama/config/SecurityConfig.java
+++ src/main/java/com/example/rama/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.ForwardedHeaderWriter;
 
 @Configuration
 public class SecurityConfig {
@@ -11,8 +12,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .headers(headers -> headers
+                .addHeaderWriter(new ForwardedHeaderWriter()) // Necesario para Render
+            )
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/images/**", "/styles/**","/test").permitAll()
+                .requestMatchers("/", "/images/**", "/styles/**", "/test").permitAll()
                 .anyRequest().authenticated()
             )
             .oauth2Login(oauth2 -> oauth2
@@ -25,4 +29,3 @@ public class SecurityConfig {
         return http.build();
     }
 }
-
